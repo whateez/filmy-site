3f2922943f0c9c09a704e720ef2bdf9ba8559b88
George <george@whateez.in>
2016-05-15 09:06:43 +0530

    Minor Change to correct model & admin

--- reviews/models.py
+++ reviews/models.py
@@ -3,19 +3,19 @@ from django.utils import timezone
 
 class review(models.Model):
     title = models.CharField(max_length=200)
-    subtitle = models.CharField(max_length=200)
-    tldr = models.CharField(max_length=200)
+    subtitle = models.CharField(blank=True, null=True, max_length=200)
+    tldr = models.CharField(blank=True, null=True, max_length=200)
     desc = models.TextField()
     author = models.ForeignKey('auth.User')
     #tags =
     #cover_image =
-    rating = models.IntegerField()
+    rating = models.IntegerField(blank=True, null=True)
     created_date = models.DateField(default=timezone.now)
     published_date = models.DateField(blank=True, null=True)
 
-def publish(self):
-    self.published_date = timezone.now()
-    self.save()
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
 
-def __str__(self):
-    return self.title
+    def __str__(self):
+        return self.title
