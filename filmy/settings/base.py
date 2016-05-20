ad33eaf77e9cb6c7db512f228166365770ca384c
George <george@whateez.in>
2016-05-20 08:42:01 +0530

    removed gunicorn, djurls, whitenoise

--- filmy/settings/base.py
+++ filmy/settings/base.py
@@ -119,8 +119,3 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
-
-import dj_database_url
-DATABASES['default'] = dj_database_url.config()
-
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
